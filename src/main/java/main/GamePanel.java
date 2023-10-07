0b8cc666bbe127083abbba5f338538372ff93aeb
jerquinigo <jonathanerquinigo@pursuit.org>
2023-10-07 03:02:45 -0400

    notes on lesson 3

--- src/main/java/main/GamePanel.java
+++ src/main/java/main/GamePanel.java
@@ -7,14 +7,22 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
-    private int xDelta = 100;
-    private int yDelta = 100;
+    private float xDelta = 100;
+    private float yDelta = 100;
+    private float xDir = 1f;
+    private float yDir = 1f;
+    private int frames = 0;
+    private long lastCheck = 0;
+    private Color color = new Color(150,20,90);
+    private Random random;
 
     public GamePanel() {
+        random = new Random();
         mouseInputs = new MouseInputs(this);
         //this adds the Gamepanel class as a parameter
         addKeyListener(new KeyboardInputs(this));
@@ -24,23 +32,51 @@ public class GamePanel extends JPanel {
 
     public void changeXDelta(int value) {
         this.xDelta = this.xDelta + value;
-        repaint();
+
     }
 
     public void changeYDelta(int value) {
         this.yDelta = this.yDelta + value;
-        repaint();
+
     }
 
     public void setRectPost(int x, int y) {
         this.xDelta = x;
         this.yDelta = y;
-        repaint();
+
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.fillRect(xDelta, yDelta,200,50);
+
+        updateRectangle();
+        g.setColor(color);
+        g.fillRect((int) xDelta,(int) yDelta,200,50);
+
+
+
+    }
+
+    private void updateRectangle() {
+        xDelta = xDelta + xDir;
+        if(xDelta > 400 || xDelta < 0) {
+            xDir = xDir * -1;
+            color = getRndColor();
+        }
+
+        yDelta = yDelta + yDir;
+        if(yDelta > 400 || yDelta < 0) {
+            yDir = yDir * -1;
+            color = getRndColor();
+        }
+    }
+
+    private Color getRndColor() {
+        int r = random.nextInt(255);
+        int b = random.nextInt(255);
+        int g = random.nextInt(255);
+
+        return new Color(r,g,b);
     }
 
 }
