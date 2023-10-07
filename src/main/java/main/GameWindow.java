7eac790816f7d0845b0800e2ea2240b9bdeb7fc8
jerquinigo <jonathanerquinigo@pursuit.org>
2023-10-07 14:24:16 -0400

    learned to get image painted in jframe and select certain image to show on screen

--- src/main/java/main/GameWindow.java
+++ src/main/java/main/GameWindow.java
@@ -6,10 +6,12 @@ public class GameWindow extends JFrame {
 
     public GameWindow(GamePanel gamePanel) {
        JFrame jFrame = new JFrame();
-       jFrame.setSize(400, 400);
        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        jFrame.add(gamePanel);
        jFrame.setLocationRelativeTo(null);
+       jFrame.setResizable(false);
+       //this means fit the size of the window to its component
+       jFrame.pack();
         jFrame.setVisible(true);
     }
 }
